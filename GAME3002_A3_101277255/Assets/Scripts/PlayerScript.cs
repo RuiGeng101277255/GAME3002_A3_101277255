fe16b15e0d0585e253d566ba2b4413cab87d2dfc
RuiGeng101277255 <ruichen.gengli@georgebrown.ca>
2021-04-20 04:10:18 -0400

    updated timer

--- GAME3002_A3_101277255/Assets/Scripts/PlayerScript.cs
+++ GAME3002_A3_101277255/Assets/Scripts/PlayerScript.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
@@ -16,6 +14,9 @@ public class PlayerScript : MonoBehaviour
     Vector3 Left = new Vector3(-1.0f, 0.0f, 0.0f);
     Vector3 Right = new Vector3(1.0f, 0.0f, 0.0f);
 
+    bool isMoveRight = true;
+    bool doneMoving = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +28,7 @@ public class PlayerScript : MonoBehaviour
     void Update()
     {
         CheckPlayerInput();
+        checkOrientation();
     }
 
     private void CheckPlayerInput()
@@ -39,10 +41,20 @@ public class PlayerScript : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             player_RB.AddForce(Left * moveSpeedRate, ForceMode.Impulse);
+            if (isMoveRight)
+            {
+                isMoveRight = false;
+                doneMoving = false;
+            }
         }
         else if (Input.GetKey(KeyCode.D))
         {
             player_RB.AddForce(Right * moveSpeedRate, ForceMode.Impulse);
+            if (!isMoveRight)
+            {
+                isMoveRight = true;
+                doneMoving = false;
+            }
         }
 
         player_Anim.SetBool("isMoving", player_RB.velocity.magnitude > 0.0f);
@@ -54,6 +66,23 @@ public class PlayerScript : MonoBehaviour
         else
         {
             player_Anim.SetBool("isJumping", false);
+        }
+    }
+
+    private void checkOrientation()
+    {
+        //Transform used only for the character to look left/right, not for physics
+        if (!doneMoving)
+        {
+            if (isMoveRight)
+            {
+                transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, Mathf.Abs(transform.localScale.z));
+            }
+            else
+            {
+                transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, -Mathf.Abs(transform.localScale.z));
+            }
+            doneMoving = true;
         }
     }
 }
--- GAME3002_A3_101277255/Assets/Scripts/TimerScript.cs
+++ GAME3002_A3_101277255/Assets/Scripts/TimerScript.cs
@@ -7,26 +7,70 @@ public class TimerScript : MonoBehaviour
     public float maxtime = 120.0f; //120 secs in 2 min
 
     TextMesh timer_txt;
+    float sec;
+    float millisec;
+    float min;
 
     // Start is called before the first frame update
     void Start()
     {
         timer_txt = GetComponent<TextMesh>();
-        getModulusMode();
+        min = 2;
+        sec = 0;
+        millisec = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        maxtime -= Time.deltaTime;
         getModulusMode();
+        maxtime -= Time.deltaTime;
     }
 
     private void getModulusMode() // Modulus arithmetic for separating min, sec and millisec from timer of unit seconds
     {
-        float sec = maxtime % 60.0f;
-        float millisec = Mathf.Abs((sec - Mathf.Round(sec)) * 100.0f); //For later to display 2 digits of the milliseconds
-        float min = (maxtime - sec) / 60.0f;
-        timer_txt.text = Mathf.Round(min) + ": " + Mathf.Round(sec) + ": " + Mathf.Round(millisec);
+        //To allow 00 to show when the value is 0, also to put a 0 infront of single digits
+        string secText = sec.ToString();
+        string millisecText = millisec.ToString();
+        string minText = min.ToString();
+
+        if ((sec < 10.0f) && (sec > 0.0f))
+        {
+            secText = "0" + sec.ToString();
+        }
+        else if (sec <= 0.0f)
+        {
+            secText = "00";
+        }
+
+        if ((millisec < 10.0f) && (millisec > 0.0f))
+        {
+            millisecText = "0" + millisec.ToString();
+        }
+        else if (millisec <= 0.0f)
+        {
+            millisecText = "00";
+        }
+
+        if ((min < 10.0f) && (min > 0.0f))
+        {
+            minText = "0" + min.ToString();
+        }
+        else if (sec <= min)
+        {
+            minText = "00";
+        }
+
+        timer_txt.text = minText + ": " + secText + ": " + millisecText;
+
+        //Updating timer part
+        sec = maxtime % 60;
+        millisec = Mathf.Abs((sec - Mathf.Round(sec)) * 100.0f); //For 2 digits of the milliseconds
+        min = (maxtime - sec) / 60.0f;
+
+        //setting 2 digits rounds
+        sec = Mathf.Round(sec);
+        millisec = Mathf.Round(millisec);
+        min = Mathf.Round(min);
     }
 }
